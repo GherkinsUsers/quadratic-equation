9f3fb9d4dda5b5381a7bb8bc73cd047fecb8d8d2
Anton <3702947@gmail.com>
2018-02-24 02:51:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var temp = equation.replace('* x^2 ', '').replace('* x ', '').replace(/\s/g, '').match(/[+ -]*\d+/g);
+  var a = temp[0], b = temp[1], c = temp[2];
+  var D = (b * b - 4 * a * c);
+  var result = [];
+  if (D > 0) {
+    result[0] = Math.round(-b - Math.sqrt(D)) / (2 * a);
+    result[1] = Math.round(-b + Math.sqrt(D)) / (2 * a);
+  }
+  else if (D = 0) {
+    result[0] = result[1] = -1 * b / (2 * a);
+  }
+  else {
+    return false;
+  }
+  sorter = function (r, y) {
+    return r - y;
+  }
+  result.sort(sorter);
+
+  return result;
 }
